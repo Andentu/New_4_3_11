63a35514b4897f4d4ef243090393709e540a4c4e
Andentu <severalmaz97@gmail.com>
2023-02-13 21:09:55 +0300

    Код для вывод массива на экран построчно

--- New_4_3_11/Program.cs
+++ New_4_3_11/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
+            int[,] array = { { 4, 6, 1, 0, 22 }, { 4, 33, 21, 1, 0 }, { 65, 1, 0, -2, 5 }, { 1, 0, 66, -87, 39 }, { 38, 0, 41, -23, -1 } };
+            for (int i = 0; i < array.GetUpperBound(0) + 1; i++)
+            {
+                for (int k = 0; k < array.GetUpperBound(1) + 1; k++)
+                {
+                    Console.Write(array[i, k] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
