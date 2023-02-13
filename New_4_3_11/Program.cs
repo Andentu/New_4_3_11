26819d346854f88e76c2569487c1ebc1722dd750
Andentu <severalmaz97@gmail.com>
2023-02-13 21:13:41 +0300

    Добавлен код для вывода массива на экран по столбцам

--- New_4_3_11/Program.cs
+++ New_4_3_11/Program.cs
@@ -5,11 +5,23 @@
         static void Main(string[] args)
         {
             int[,] array = { { 4, 6, 1, 0, 22 }, { 4, 33, 21, 1, 0 }, { 65, 1, 0, -2, 5 }, { 1, 0, 66, -87, 39 }, { 38, 0, 41, -23, -1 } };
+
+            Console.WriteLine("Вывод массива на экран построчно: ");
             for (int i = 0; i < array.GetUpperBound(0) + 1; i++)
             {
                 for (int k = 0; k < array.GetUpperBound(1) + 1; k++)
                 {
                     Console.Write(array[i, k] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nВывод массива на экран по столбцам: ");
+            for (int i = 0; i < array.GetUpperBound(1) + 1; i++)
+            {
+                for (int k = 0; k < array.GetUpperBound(0) + 1; k++)
+                {
+                    Console.Write(array[k, i] + " ");
                 }
                 Console.WriteLine();
             }
